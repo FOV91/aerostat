09a0f7d481d5ed9b350eb33f2bb132c3b3bb8c38
FOV91 <fovspb@mail.ru>
2017-05-26 13:01:17 +0300

    Бургер готов

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
   // весь ваш код
- 
+ var menu = document.querySelector('.menu');
+function toggleMenu () {
+  menu.classList.toggle('open');
+}
+menu.addEventListener('click', toggleMenu);
 });
