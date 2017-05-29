7963c74d67e47a677429cb79556a78a43e86b39b
FOV91 <fovspb@mail.ru>
2017-05-29 22:46:51 +0300

    Блок Галерея 320

--- src/js/script.js
+++ src/js/script.js
@@ -5,4 +5,34 @@ function toggleMenu () {
   menu.classList.toggle('open');
 }
 menu.addEventListener('click', toggleMenu);
+
+});
+
+
+	$('.gallery__slider').slick({
+		
+			prevArrow: '<img class="prevArrow" src="img/ArrowPrew.png" width="40px" height="48px">',
+		  nextArrow: '<img class="nextArrow" src="img/ArrowNext.png" width="40px" height="48px">',
+
+  	 	dots: false,
+		  slidesToShow: 2,
+		  slidesToScroll: 1,
+		  responsive: [
+		    {
+		      breakpoint: 1280,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 768,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  ]
+  		
 });
