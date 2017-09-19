4328ee3f73e232ad5ef9195b63c9c3131428e506
FOV91 <fovspb@mail.ru>
2017-09-19 18:32:20 +0300

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -16,6 +16,34 @@ menu.addEventListener('click', toggleMenu);
     return false;
   });
 
+
+	$(function() {
+ 
+$(window).scroll(function() {
+ 
+if($(this).scrollTop() != 0) {
+ 
+$('#toTop').fadeIn();
+ 
+} else {
+ 
+$('#toTop').fadeOut();
+ 
+}
+ 
+});
+ 
+$('#toTop').click(function() {
+ 
+$('body,html').animate({scrollTop:0},800);
+ 
+});
+ 
+});
+
+
+  
+
 	$('.reviews-slider').slick({
 		prevArrow: '<img class="prevArrowRev" src="img/ArrowNextRev.png" width="40px" height="48px">',
 		nextArrow: '<img class="nextArrowRev" src="img/ArrowNextRev.png" width="40px" height="48px">',
